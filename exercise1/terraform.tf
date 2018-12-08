5688071c0c8b95dcb04bab422e31f245e015230c
abhishekash <aash@equalexperts.com>
2018-12-08 14:21:51 +0530

    Asgard infra

--- exercise1/terraform.tf
+++ exercise1/terraform.tf
@@ -1,9 +1,10 @@
+
 terraform {
   required_version = "= 0.11.10"
 
   backend "s3" {
     bucket = "ecs-workshop-terraform-state-dev"
-    key = "${unique_name}-cluster-dev.tfstate"
+    key = "abhishek-cluster-dev.tfstate"
     dynamodb_table = "Terraform-Lock-Table"
     encrypt = true
     region = "us-east-1"
